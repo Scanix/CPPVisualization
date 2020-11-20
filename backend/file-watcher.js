2eeb6670fdf7bff79ba737e995ef4d2983c57bf6
Blatoy <lock.jeanneret@gmail.com>
2020-11-20 16:01:53 +0100

    Add json file bridge

---
+++ backend/file-watcher.js
@@ -0,0 +1,35 @@
+const { ipcMain, remote, dialog, BrowserWindow } = require("electron");
+const tempJson = require("./demo-structure.json");
+
+module.exports.addEvents = function () {
+    ipcMain.on("open-project-picker", (event, arg) => {
+        dialog.showOpenDialog({
+            properties: ["openDirectory"]
+        }).then((files) => {
+            if (files !== undefined) {
+                watchProject(files);
+            }
+        });
+    })
+}
+
+/**
+ * Watch folder structure and parse cpp files
+ * @param {string} path 
+ */
+function watchProject(path) {
+    // TODO: This should watch and parse project, for now it just sends dummy data
+    sendProjectStructure(tempJson);
+
+    setInterval(() => {
+        sendProjectStructure(tempJson);
+    }, 5000);
+}
+
+/**
+ * Send object to frontend
+ * @param {object} obj 
+ */
+function sendProjectStructure(obj) {
+    BrowserWindow.getFocusedWindow().webContents.send("project-structure-updated", JSON.stringify(obj));
+}
---
+++ src/project-structure-loader.js
@@ -0,0 +1,33 @@
+const { ipcRenderer } = require('electron');
+
+// Note: this class could be swapped by something else if we just want to load another json
+// Note: this class should be a singleton with the current way it works
+class ProjectStructureLoader {
+    /**
+     * @param {function} onProjectLoaded Callback called every time project structure is updated
+     */
+    constructor(onProjectLoaded) {
+        ipcRenderer.on("project-structure-updated", (event, arg) => {
+            onProjectLoaded(JSON.parse(arg));
+        });
+    }
+
+    /**
+     * Open file picker
+     */
+    pickFile() {
+        ipcRenderer.send("open-project-picker");
+    }
+
+}
+
+/*
+Exemple usage:
+const projectStructureLoader = new ProjectStructureLoader((json) => {
+    // Do whatever with json file
+    console.log(json);
+});
+
+// Call when "open folder" is clicked
+projectStructureLoader.pickFile();
+*/

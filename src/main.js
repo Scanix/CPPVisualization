6afd07326d5ff6314f54cdcb83cfa463601fbeb8
Blatoy <lock.jeanneret@gmail.com>
2020-11-26 11:56:54 +0100

    Fix code style

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import ProjectStructureLoader from './project-structure-loader.js';
 
-export default function selectFile() {
-	
-	var projectStructureLoader = new ProjectStructureLoader((json) => {
-   
-	// Do whatever with json file
-    console.log(json);});
-	
-	projectStructureLoader.pickFile();
-}
+const projectStructureLoader = new ProjectStructureLoader((json) => {
+    // Do whatever with json file
+    console.log(json);
+});
+
+// Add events
+document.getElementById("browse-project").addEventListener("click", projectStructureLoader.pickFile);
